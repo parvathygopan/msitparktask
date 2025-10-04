9a76f4d4c7c7bb93c741811620397f8fcc2bae21
parvathygopan <parvathy671999@gmail.com>
2025-10-04 17:52:53 +0530

    corrected

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BiHome } from 'react-icons/bi'
 import "./BannerStyle.css"
 import Service from './Service'
+import BannerHouse from "../../Assets/house.png"
 function Banner() {
   return (
    <section className="banner-section">
@@ -10,7 +11,12 @@ function Banner() {
           <h1 className="banner-title">
             Home Experts at 
           </h1>
-          <h1 className='banner-title2'>Your <BiHome className="house-icon" /> Door</h1>
+          <h1 className='banner-title2'>Your <img
+                    src={BannerHouse}
+                      alt="banner"
+                      className="house-icon"
+                      draggable="false"
+                    /> Door</h1>
           <p className="banner-subtitle">
             Expert home services, delivered to your doorstep - get reliable handyman solution
             with ease. Fast, professional, and tailored to your needs.
